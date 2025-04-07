84d0541a17304ee5b6be088344495c3febe3409c
unknown <enmanuelgutierrezpoey@gmail.com>
2025-04-07 02:41:29 -0400

    "Appling renderingHints and resizing image to fit ImageDealer's borders" 	modified:   java/galli/image/ImageDealer.java

--- java/galli/image/ImageDealer.java
+++ java/galli/image/ImageDealer.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ImageDealer extends JPanel {
-	int index = 0;
-	BufferedImage image;
-	ArrayList<File> imageList;
-	
+	private int index = 0;
+	private BufferedImage image;
+	private ArrayList<File> imageList;
+	private Dimension lastSize = new Dimension();
 	public ImageDealer(ArrayList<File> imageList) {
 		this.imageList = imageList;
 		setSize(500, 500);
@@ -22,6 +22,10 @@ public class ImageDealer extends JPanel {
 		addComponentListener( new ComponentAdapter() {
 			@Override
 			public void componentResized( ComponentEvent e) {
+				if (!lastSize.equals( getSize() ) ) {
+					lastSize.setSize( getSize() );
+					repaint();
+				}
 			}
 		} );
 	}
@@ -30,7 +34,35 @@ public class ImageDealer extends JPanel {
 		super.paintComponent(g);
 		if (image == null) return ;
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawImage(image, 0, 0, getWidth(), getHeight(), null );
+		
+		g2d.setRenderingHint (
+			RenderingHints.KEY_INTERPOLATION,
+			RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2d.setRenderingHint (
+			RenderingHints.KEY_ANTIALIASING,
+			RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint (
+			RenderingHints.KEY_RENDERING,
+			RenderingHints.VALUE_RENDER_QUALITY);
+		
+		int panelWidth = getWidth();
+		int panelHeight = getHeight();
+		
+		double widthRadio = (double) panelWidth / image.getWidth();
+		double heightRadio = (double) panelHeight / image.getHeight();
+		double scale = Math.min(widthRadio, heightRadio);
+		
+		int scaledWidth = (int)(image.getWidth() * scale);
+		int scaledHeight = (int)(image.getHeight() * scale);
+		
+		int x = (panelWidth - scaledWidth) / 2;
+		int y = (panelHeight - scaledHeight) / 2;
+		
+		g2d.drawImage(image, 
+			x, y, x + scaledWidth, y + scaledHeight,
+			0, 0, image.getWidth(), image.getHeight(),
+			null );
+		
 	}
 	public void setImage(File file) {
 		try {
